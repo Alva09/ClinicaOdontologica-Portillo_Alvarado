1ad8ec7238820cadbabc0d53954ffe6e4d61c17c
Alva09 <daniel.alva2111@gmail.com>
2024-09-04 21:17:40 -0500

    Controller, dto, entity, exception and pom

---
+++ src/main/java/com/dh/clinicaodontologica/controller/OdontologoController.java
@@ -0,0 +1,78 @@
+package com.dh.clinicaodontologica.controller;
+
+import com.dh.clinicaodontologica.entity.Odontologo;
+import com.dh.clinicaodontologica.exception.MatriculaAlreadyRegisteredException;
+import com.dh.clinicaodontologica.exception.ResourceNotFoundException;
+import com.dh.clinicaodontologica.service.OdontologoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/odontologos")
+public class OdontologoController {
+    @Autowired
+    private OdontologoService odontologoService;
+
+    @PostMapping
+    public ResponseEntity<Odontologo> guardarOdontologo(@RequestBody Odontologo odontologo) throws MatriculaAlreadyRegisteredException {
+        if(odontologoService.existeOdontologoConMatricula(odontologo.getMatricula())){
+            throw new MatriculaAlreadyRegisteredException();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(odontologoService.guardarOdontologo(odontologo));
+    }
+
+    @GetMapping("/buscar-id/{id}")
+    public ResponseEntity<Optional<Odontologo>> buscarPorID(@PathVariable Long id) throws ResourceNotFoundException {
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorID(id);
+        if(!odontologoBuscado.isPresent()){
+            throw new ResourceNotFoundException("Odontólogo no encontrado");
+        }
+        return ResponseEntity.ok(odontologoBuscado);
+    }
+
+    @GetMapping("/buscar-matricula/{matricula}")
+    public ResponseEntity<Optional<Odontologo>> buscarPorMatricula(@PathVariable String matricula) throws ResourceNotFoundException{
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorMatricula(matricula);
+        if(!odontologoBuscado.isPresent()){
+            throw new ResourceNotFoundException("Odontólogo no encontrado");
+        }
+        return ResponseEntity.ok(odontologoBuscado);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> actualizarOdontologo(@RequestBody Odontologo odontologo) throws ResourceNotFoundException, MatriculaAlreadyRegisteredException{
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorID(odontologo.getId());
+        if (odontologoBuscado.isPresent()) {
+            if (odontologoService.existeOdontologoConMatricula(odontologo.getMatricula()) &&
+                    !odontologoBuscado.get().getMatricula().equals(odontologo.getMatricula())) {
+                throw new MatriculaAlreadyRegisteredException();
+            } else {
+                odontologoService.actualizarOdontologo(odontologo);
+                return ResponseEntity.ok("Odontologo actualizado exitosamente");
+            }
+        } else {
+            throw new ResourceNotFoundException("Odontólogo no encontrado");
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarOdontologo(@PathVariable Long id) throws ResourceNotFoundException{
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorID(id);
+        if(!odontologoBuscado.isPresent()){
+            throw new ResourceNotFoundException("Odontólogo no encontrado");
+        }
+        odontologoService.eliminarOdontologo(id);
+        return ResponseEntity.ok("Odontologo actualizado exitosamente");
+    }
+
+    @GetMapping("/listar-todos")
+    public ResponseEntity<List<Odontologo>> listarTodos(){
+        return ResponseEntity.ok(odontologoService.listarTodos());
+    }
+}
---
+++ src/main/java/com/dh/clinicaodontologica/entity/Odontologo.java
@@ -0,0 +1,31 @@
+package com.dh.clinicaodontologica.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "odontologos")
+public class Odontologo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String matricula;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+
+    public Odontologo(String matricula, String nombre, String apellido) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
